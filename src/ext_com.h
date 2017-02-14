c18a023e830b16cbc737bb02f6580fe1e9d8421d
asctec-andre <andre.ryll@asctec.de>
2017-02-14 11:01:40 +0100

    Modified license headers

--- src/ext_com.h
+++ src/ext_com.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Ascending Technologies GmbH, Germany
+ * Copyright (C) 2017 Intel Deutschland GmbH, Germany
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
